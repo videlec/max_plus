2d26dae8be43513331b5f0719a2a2ae4b3fd5f13
Vincent Delecroix <vincent.delecroix@labri.fr>
2016-04-26 09:25:30 -0300

    sv_candidates

--- bin/sv_candidates.py
+++ bin/sv_candidates.py
@@ -13,6 +13,9 @@ d = int(sys.argv[2])
 
 assert n > 0 and d > 0
 
+def word_nice_str(u):
+    return ''.join(map(str,u))
+
 def word_rank(u):
     return sum(2**i for i,j in enumerate(u) if j)
 
@@ -51,7 +54,7 @@ i = int(i)
 if i < 0 or i >= nb_tasks:
     raise RuntimeError("i = {} should be in between 0 and nb_task={}".format(i, nb_tasks))
 
-i_start, i_stop = get_task(i, 0, 2**n-1, nb_tasks)
+i_start, i_stop = get_task(i, 2**(n-1), 2**n-1, nb_tasks)
 u_start = word_unrank(i_start, n)
 u_stop   = word_unrank(i_stop ,n)
 
@@ -59,10 +62,10 @@ from max_plus.sv_identities import sv_candidates
 
 JOB_ID = os.getenv('SLURM_JOB_ID')
 if JOB_ID is None:
-    raise RuntimeError("no job id")
+    raise RuntimeError("SLURM_JOB_ID not available")
 
 descr = '# sv candidates n={} d={} u_start={} u_stop={} #'.format(
-        n, d, u_start, u_stop)
+        n, d, word_nice_str(u_start), word_nice_str(u_stop))
 slurm  = '# JOB_ID: {}'.format(JOB_ID)
 slurm += ' '*(len(descr)-len(slurm)-1) + '#\n'
 t = '# NTASKS: {}'.format(nb_tasks)
@@ -74,4 +77,8 @@ print descr
 print slurm
 print '#'*len(descr)
 for u in sv_candidates(n, d, u_start, u_stop):
-    print u
+    print '{} {}'.format(word_nice_str(u[0]), word_nice_str(u[1]))
+print '#'*len(descr)
+slurm = '# END'
+print slurm + ' ' *(len(descr)-len(t)-1) + '#'
+print '#'*len(descr)
