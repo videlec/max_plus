f6eaaed87b37d83f019a14c50d9839610c4c07aa
Vincent Delecroix <vincent.delecroix@labri.fr>
2016-04-26 14:16:28 -0300

    smarter sv_filter

--- bin/sv_filter.py
+++ bin/sv_filter.py
@@ -11,27 +11,40 @@ d = int(sys.argv[2])
 indir = 'sv_candidates/{}/{}'.format(d,n)
 
 candidates = []
-print "reading files..."
-for filename in os.listdir(indir):
+files = os.listdir(indir)
+print "reading {} files...".format(len(files)),
+n_errors = 0
+not_finished = 0
+for filename in files:
     filename = os.path.join(indir,filename)
     if os.path.isfile(filename):
+        not_end = True
         with open(filename) as f:
             line = f.readline()
             while line:
                 if not line.startswith('#'):
-                    t0,t1 = line.split()
-                    assert len(t0) == len(t1) == n, (t0, t1)
-                    candidates.append((map(int,t0), map(int,t1)))
+                    line = line.split()
+                    if len(line) != 2 or len(line[0]) != n or len(line[1]) != n:
+                        n_errors += 1
+                    else:
+                        t0,t1 = line
+                        candidates.append((map(int,t0), map(int,t1)))
+                elif 'END' in line:
+                    not_end = False
                 line = f.readline()
-candidates.sort()
+            not_finished += not_end
 print "done"
-print 'n = {}  d = {}'.format(n,d)
+candidates.sort()
 print '{} candidates'.format(len(candidates))
+if n_errors:
+    print "-> {} malformated lines".format(n_errors)
+if not_finished:
+    print "-> {} computation not terminated".format(not_finished)
 num=0
 output = open(os.path.join('sv_identities/{}_{}'.format(d,n)), 'w')
 for t1,t2 in candidates:
     if is_sv_identity(t1,t2,d):
         output.write('{} {}\n'.format(''.join(map(str,t1)),''.join(map(str,t2))))
         num += 1
-print 'got {} identities'.format(num)
+print '{} identities'.format(num)
 output.close()
