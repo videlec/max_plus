8e8cf7d1807d85e70a1021b433da88abde33dc4b
Vincent Delecroix <vincent.delecroix@labri.fr>
2016-04-26 12:06:51 -0300

    sv_candidates / sv_filter

--- bin/sv_candidates.py
+++ bin/sv_candidates.py
@@ -1,18 +1,3 @@
-##########################
-# BEGINING OF THE SCRIPT #
-##########################
-
-import sage.all
-import sys
-import os
-
-if len(sys.argv) != 3:
-    raise RuntimeError("usage: sv_candidates n d")
-n = int(sys.argv[1])
-d = int(sys.argv[2])
-
-assert n > 0 and d > 0
-
 def word_nice_str(u):
     return ''.join(map(str,u))
 
@@ -37,48 +22,97 @@ def get_task(i, start, end, k):
     r = (end-start+1) % k
 
     if i < r:
-        return (start + (q+1)*i, start + (q+1)*(i+1) - 1)
+        out = (start + (q+1)*i, start + (q+1)*(i+1) - 1)
     else:
-        return (start + (q+1)*r + q*(i-r), start + (q+1)*r + q*(i-r+1) - 1)
+        out = (start + (q+1)*r + q*(i-r), start + (q+1)*r + q*(i-r+1) - 1)
 
-nb_tasks = os.getenv('SLURM_NTASKS')
-if nb_tasks is None:
-    raise RuntimeError("SLURM_NTASKS not available")
-nb_tasks = int(nb_tasks)
+    print "get_task({}, {}, {}, {}) -> {}".format(i, start, end, k, out)
+    return out
 
-i = os.getenv('SLURM_PROCID')
-if i is None:
-    raise RuntimeError("SLURM_PROCID not available")
-i = int(i)
+def write_line(f, s):
+    if s is None:
+        f.write('#'*73)
+        f.write('\n')
+    else:
+        f.write('# ')
+        f.write(s)
+        f.write(' '*(70-len(s)))
+        f.write('#\n')
 
-if i < 0 or i >= nb_tasks:
-    raise RuntimeError("i = {} should be in between 0 and nb_task={}".format(i, nb_tasks))
+def run_task(arg):
+    n, d, i_start, i_stop, outdir, jobid = arg
 
-i_start, i_stop = get_task(i, 2**(n-1), 2**n-1, nb_tasks)
-u_start = word_unrank(i_start, n)
-u_stop   = word_unrank(i_stop ,n)
+    import sage.all
+    from max_plus.sv_identities import sv_candidates
 
-from max_plus.sv_identities import sv_candidates
+    u_start = word_unrank(i_start, n)
+    u_stop = word_unrank(i_stop, n)
 
-JOB_ID = os.getenv('SLURM_JOB_ID')
-if JOB_ID is None:
-    raise RuntimeError("SLURM_JOB_ID not available")
+    p = mp.current_process()
+    s_start = word_nice_str(u_start)
+    s_stop = word_nice_str(u_stop)
+    f = open(os.path.join(outdir, '{}-{}'.format(s_start, s_stop)), 'w')
 
-descr = '# sv candidates n={} d={} u_start={} u_stop={} #'.format(
-        n, d, word_nice_str(u_start), word_nice_str(u_stop))
-slurm  = '# JOB_ID: {}'.format(JOB_ID)
-slurm += ' '*(len(descr)-len(slurm)-1) + '#\n'
-t = '# NTASKS: {}'.format(nb_tasks)
-slurm += t + ' '*(len(descr)-len(t)-1) + '#\n'
-t = '# PROCID: {}'.format(i)
-slurm += t + ' '*(len(descr)-len(t)-1) + '#'
-print '#'*len(descr)
-print descr
-print slurm
-print '#'*len(descr)
-for u in sv_candidates(n, d, u_start, u_stop):
-    print '{} {}'.format(word_nice_str(u[0]), word_nice_str(u[1]))
-print '#'*len(descr)
-slurm = '# END'
-print slurm + ' ' *(len(descr)-len(t)-1) + '#'
-print '#'*len(descr)
+    write_line(f, None)
+    write_line(f, 'sv candidates n={} d={}'.format(n,d))
+    write_line(f, 'u_start: {} ({})'.format(s_start, i_start))
+    write_line(f, 'u_stop : {} ({})'.format(s_stop, i_stop))
+    write_line(f, 'JOB_ID : {}'.format(jobid))
+    write_line(f, 'NTASKS : {}'.format(nb_tasks))
+    write_line(f, 'PROCID : {}'.format(i))
+    write_line(f, p.name)
+    write_line(f, None)
+    for u in sv_candidates(n, d, u_start, u_stop):
+        f.write('{} {}\n'.format(word_nice_str(u[0]), word_nice_str(u[1])))
+    write_line(f, None)
+    write_line(f, 'END')
+    write_line(f, None)
+    f.close()
+
+if __name__ == '__main__':
+    import sys
+    import os
+    import multiprocessing as mp
+
+    if len(sys.argv) != 3:
+        raise RuntimeError("usage: sv_candidates n d")
+    n = int(sys.argv[1])
+    d = int(sys.argv[2])
+
+    assert n > 0 and d > 0
+
+    nb_tasks = os.getenv('SLURM_NTASKS')
+    if nb_tasks is None:
+        raise RuntimeError("SLURM_NTASKS not available")
+    nb_tasks = int(nb_tasks)
+
+    i = os.getenv('SLURM_PROCID')
+    if i is None:
+        raise RuntimeError("SLURM_PROCID not available")
+    i = int(i)
+
+    jobid = os.getenv('SLURM_JOB_ID')
+    if jobid is None:
+        raise RuntimeError("SLURM_JOB_ID not available")
+
+    if i < 0 or i >= nb_tasks:
+        raise RuntimeError("i = {} should be in between 0 and nb_task={}".format(i, nb_tasks))
+
+    ncpus = mp.cpu_count()
+
+    outdir = os.path.join('sv_candidates', str(d), str(n))
+
+    # (i_start, i_stop) is the interval allocated to this task
+    # we further divide it according to the number of cpus available
+    i_start, i_stop = get_task(i, 2**(n-1), 2**n-1, nb_tasks)
+    tasks = []
+    nb_subtasks = 5*ncpus
+    for j in xrange(nb_subtasks):
+        i0,i1 = get_task(j, i_start, i_stop, nb_subtasks)
+        tasks.append((n, d, i0, i1, outdir, jobid))
+
+    pool = mp.Pool(ncpus)
+    for _ in pool.imap_unordered(run_task, tasks):
+        pass
+    pool.terminate()
+    pool.join()
--- bin/sv_filter.py
+++ bin/sv_filter.py
@@ -1,3 +1,4 @@
+import sage.all
 import sys
 import os
 from max_plus import is_sv_identity
@@ -7,17 +8,29 @@ if len(sys.argv) != 3:
 n = int(sys.argv[1])
 d = int(sys.argv[2])
 
-candidates = []
-for filename in os.listdir('sv_relations/{}/{}'.format(d,n)):
-    print "reading {}".format(filename)
-    if os.path.isfile(filename):
-        with open('filename') as f:
-            lines = [line for line in f.read().split('\n') if line and not line.startsiwth('#')]
-        candidates.append(map(eval,lines))
+indir = 'sv_candidates/{}/{}'.format(d,n)
 
-print '# n = {}  d = {}'.format(n,d)
-print '# {} candidates'.format(len(candidates))
+candidates = []
+print "reading files..."
+for filename in os.listdir(indir):
+    filename = os.path.join(indir,filename)
+    if os.path.isfile(filename):
+        with open(filename) as f:
+            line = f.readline()
+            while line:
+                if not line.startswith('#'):
+                    t0,t1 = line.split()
+                    assert len(t0) == len(t1) == n, (t0, t1)
+                    candidates.append((map(int,t0), map(int,t1)))
+                line = f.readline()
+print "done"
+print 'n = {}  d = {}'.format(n,d)
+print '{} candidates'.format(len(candidates))
+num=0
+output = open(os.path.join('sv_identities/{}_{}'.format(n,d)), 'w')
 for t1,t2 in candidates:
     if is_sv_identity(t1,t2,d):
-        print t1,t2
-
+        output.write('{} {}\n'.format(''.join(map(str,t1)),''.join(map(str,t2))))
+        num += 1
+print 'got {} identities'.format(num)
+output.close()
